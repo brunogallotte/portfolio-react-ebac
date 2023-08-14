00529f4ba18f945744a27cad1d58869ac40c6fbf
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-08-14 17:00:00 -0300

    Construindo a estrutura e a "seção sobre mim"

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,20 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo illum vero
+      iusto! Eius earum, consectetur sed nisi pariatur libero fuga porro illo
+      quas, perspiciatis, dolores molestiae tempore officiis ratione obcaecati!
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=brunogallotte&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=brunogallotte&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
