a2198ddfec8e348982ad98bf1ef2baf32a0c040b
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-08-14 19:19:56 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/brunogallotte.png"></img>
-    <Titulo fontSize={20}>Teste</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bruno Gallotte</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @brunogallotte
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Fullstack Developer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
